2d1462aacfd749b0d5e596bb28d4f3a25e79e884
Chad Watson <chad@40digits.com>
2015-04-30 22:20:30 -0500

    Adding production files.

--- sassqwatch.js
+++ sassqwatch.js
@@ -205,6 +205,8 @@ module.exports = function(options) {
 
   // Listen for media query changes
   sassqwatch.onMediaQueryChange(updateImages);
+
+  return sassqwatch;
 };
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./elementify":1,"./preloader":2,"./sassqwatch":"sassqwatch","./toDashed":5}],4:[function(require,module,exports){
@@ -310,14 +312,10 @@ module.exports = function(string) {
 };
 },{}],"sassqwatch":[function(require,module,exports){
 (function (global){
-
 // Dependencies
 var
   $ = (typeof window !== "undefined" ? window.$ : typeof global !== "undefined" ? global.$ : null),
   td = require('./throttleDebounce');
-
-// SassQwatch object
-var sassqwatch = {};
 
 // Elements
 var
@@ -344,7 +342,7 @@ var matches = function(which) {
 /**
  * Internal: When the browser is resized, update the media query
  */
-var onResize = function () {
+var onResize = function() {
   var lastMediaQuery = currentMediaQuery;
 
   // Set the global current media query
@@ -360,7 +358,7 @@ var onResize = function () {
 /**
  * Internal: Sets the order of media queries
  */
-var setOrder = function () {
+var setOrder = function() {
   var mediaQueries = $orderElement.css('font-family');
   mqOrderNumbered = mediaQueries.replace(/['"\s]/g, "").split(',');
 
@@ -369,15 +367,67 @@ var setOrder = function () {
   });
 };
 
+/**
+ * Public: Event fires when the media query changes
+ * @param callback - The function to call when the event is fired
+ */
 var onMediaQueryChange = function(callback) {
   $listenElement.on('mediaQueryChange', function(e, newMediaQuery, oldMediaQuery) {
     callback(newMediaQuery, oldMediaQuery);
   });
+  return this;
+};
+
+/**
+ * Public: Event fires on the specified media query
+ * @param which - The media query to check against
+ * @param callback - The function to call when the event is fired
+ */
+var onMediaQuery = function(which, callback) {
+  $listenElement.on('mediaQueryChange', function(e, newMediaQuery, oldMediaQuery) {
+    if (matches(which)) {
+      callback(oldMediaQuery);
+    }
+  });
+  return this;
+};
+
+/**
+ * Public: A convenience method to fire a callback when above or below a specified breakpoint
+ * @param direction - A string "above" or "below"
+ * @param which - The media query to check against
+ * @param callback - The function to call when the event is fired
+ */
+var when = function(direction, which, callback) {
+  var
+    dir = direction.toLowerCase(),
+    check;
+
+  if (dir === 'above') {
+    check = function() {
+      if (isAbove(which)) {
+        callback();
+      }
+    }
+  } else if (dir === 'below') {
+    check = function() {
+      if (isBelow(which)) {
+        callback();
+      }
+    }
+  }
+
+  if (typeof check === 'function') {
+    check();
+    $listenElement.on('mediaQueryChange', check);
+  }
+
+  return this;
 };
 
 /**
  * Public: Checks if the media query is greater than the specified
- * @param which The media query to check against
+ * @param which - The media query to check against
  */
 var isAbove = function (which) {
   var currentMq = mqOrderNamed[fetchMediaQuery()],
@@ -388,7 +438,7 @@ var isAbove = function (which) {
 
 /**
  * Public: Checks if the media query is less than the specified
- * @param which The media query to check against
+ * @param which - The media query to check against
  */
 var isBelow = function (which) {
   var currentMq = mqOrderNamed[fetchMediaQuery()],
@@ -412,7 +462,7 @@ var fetchMediaQuery = function() {
 
 /**
  * Public: Fetch the index of a named breakpoint
- * @param mediaQuery A string referencing the desired breakpoint
+ * @param mediaQuery - A string referencing the desired breakpoint
  */
 var fetchMqIndex = function(mediaQuery) {
   return mqOrderNamed[mediaQuery];
@@ -420,7 +470,7 @@ var fetchMqIndex = function(mediaQuery) {
 
 /**
  * Public: Fetch the name of a breakpoint by its index
- * @param index An integer referencing the desired breakpoint in the order
+ * @param index - An integer referencing the desired breakpoint in the order
  */
 var fetchMqName = function(index) {
   return mqOrderNumbered[index];
@@ -428,6 +478,7 @@ var fetchMqName = function(index) {
 
 /**
  * Public: Turn throttling ON for more effecient resizing events
+ * @param duration - An integer specifying the duration of the throttling
  */
 var throttleOn = function(duration) {
   $window.on('resize.throttle', td.throttle(duration || 250, onResize));
@@ -450,9 +501,10 @@ var throttleOff = function() {
 var responsiveImages = require('./responsiveImages');
 
 /**
- * Set everything up
+ * Internal: Immediately invoked constructor function
+ * Sets everything up and then returns all the public methods.
  */
-(function() {
+var constructor = function() {
   // set the order of the breakpoints
   setOrder();
 
@@ -462,20 +514,22 @@ var responsiveImages = require('./responsiveImages');
   // no resize throttling by default
   throttleOff();
 
-  // bundle up the public properties & methods
-  sassqwatch = {
+  // return the public methods
+  return {
     onMediaQueryChange: onMediaQueryChange,
-    fetchMediaQuery: fetchMediaQuery,
-    fetchMqIndex: fetchMqIndex,
-    fetchMqName: fetchMqName,
-    isAbove: isAbove,
-    isBelow: isBelow,
-    throttleOn: throttleOn,
-    throttleOff: throttleOff,
-    responsiveImages: responsiveImages
+    onMediaQuery:       onMediaQuery,
+    when:               when,
+    fetchMediaQuery:    fetchMediaQuery,
+    fetchMqIndex:       fetchMqIndex,
+    fetchMqName:        fetchMqName,
+    isAbove:            isAbove,
+    isBelow:            isBelow,
+    throttleOn:         throttleOn,
+    throttleOff:        throttleOff,
+    responsiveImages:   responsiveImages
   };
+}.call();
 
-  module.exports = sassqwatch;
-})();
+module.exports = constructor;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./responsiveImages":3,"./throttleDebounce":4}]},{},[]);
