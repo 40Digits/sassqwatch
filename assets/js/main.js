6a3d7721c13dda36fd1469ac9d345d9b9a073a45
Chad Watson <chad@40digits.com>
2015-09-08 23:12:51 -0500

    fixes another relative path issue concerning some images on the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -358,23 +358,23 @@ module.exports = [
   {
     method: 'responsiveImages',
     scale: false,
-    initial: '/assets/images/480x270.jpg',
+    initial: 'assets/images/480x270.jpg',
     images: {
-      'tiny': '/assets/images/480x270.jpg',
-      'tiny-2x': '/assets/images/480x270-retina.jpg',
-      'small': '/assets/images/800x450.jpg',
-      'small-2x': '/assets/images/800x450-retina.jpg',
-      'large': '/assets/images/1024x575.jpg',
-      'large-2x': '/assets/images/1024x575-retina.jpg',
+      'tiny': 'assets/images/480x270.jpg',
+      'tiny-2x': 'assets/images/480x270-retina.jpg',
+      'small': 'assets/images/800x450.jpg',
+      'small-2x': 'assets/images/800x450-retina.jpg',
+      'large': 'assets/images/1024x575.jpg',
+      'large-2x': 'assets/images/1024x575-retina.jpg',
     },
     js: 'sassqwatch.responsiveImages();',
     html: '<img\n' +
-      '  data-tiny="/assets/images/480x270.jpg"\n' + 
-      '  data-tiny-2x="/assets/images/480x270-retina.jpg"\n' + 
-      '  data-small="/assets/images/800x450.jpg"\n' + 
-      '  data-small-2x="/assets/images/800x450-retina.jpg"\n' + 
-      '  data-large="/assets/images/1024x575.jpg"\n' + 
-      '  data-large-2x="/assets/images/1024x575-retina.jpg" />'
+      '  data-tiny="assets/images/480x270.jpg"\n' + 
+      '  data-tiny-2x="assets/images/480x270-retina.jpg"\n' + 
+      '  data-small="assets/images/800x450.jpg"\n' + 
+      '  data-small-2x="assets/images/800x450-retina.jpg"\n' + 
+      '  data-large="assets/images/1024x575.jpg"\n' + 
+      '  data-large-2x="assets/images/1024x575-retina.jpg" />'
   }
 ];
 
